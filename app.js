09f2bffff3df9298164408a209dfc59a89514305
ray1025t <a7807230@gmail.com>
2021-10-14 12:45:25 +0800

    feat: hbs render index

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const exphbs = require('express-handlebars')
+
+// 使用handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main' , extname: '.hbs'}))
+app.set('view engine', 'hbs' )
+
+// 路由設置
 app.get('/', (req, res) => {
-  res.send('welcome')
+  res.render('index')
 })
 
+// 路由監聽
 app.listen(port, () => {
   console.log(`express on http://localhost:${port}`)
 })
